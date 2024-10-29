2943824f0f05261a228aaeec411c3d3f8d1579e3
Michael Connor Buchan <mikey@blindcomputing.org>
2024-10-29 17:15:01 +0000

    feat: Create Engine root class
    This class encapsulates the entire engine. Currently, we initialize SDL, create a window, and poll for events. Only SDL_EVENT_QUIT is implemented, and terminates the engine.

---
+++ src/engine.cpp
@@ -0,0 +1,59 @@
+#include "engine.hpp"
+
+#include <stdexcept>
+
+#include <SDL3/SDL_init.h>
+
+Engine::Engine(int argc, char *argv[])
+{
+    // Initialize SDL. We specifically need the video subsystem, which facilitates creating windows.
+    if (!SDL_Init(SDL_INIT_VIDEO))
+    {
+        throw std::runtime_error(SDL_GetError());
+    }
+
+    // Create the main window
+    window_ = SDL_CreateWindow("Stirling Engine", DEFAULT_WIDTH, DEFAULT_HEIGHT, 0);
+    if (window_ == nullptr)
+    {
+        throw std::runtime_error(SDL_GetError());
+    }
+}
+
+Engine::~Engine()
+{
+    if (window_)
+    {
+        SDL_DestroyWindow(window_);
+    }
+
+    SDL_Quit();
+}
+
+void Engine::run()
+{
+    while (true)
+    {
+        // Process all pending events
+        // See the documentation of SDL_PollEvent() for more information
+        for (SDL_Event e; SDL_PollEvent(&e);)
+        {
+            // Handle event
+            if (!onEvent(e))
+            {
+                return; // onEvent() returned false, we should terminate
+            }
+        }
+    }
+}
+
+bool Engine::onEvent(SDL_Event &e)
+{
+    switch (e.type)
+    {
+    case SDL_EVENT_QUIT:
+        // User closed the window
+        return false; // Stop running
+    }
+    return true; // Continue running
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
+/**
+ * @file main.cpp
+ * @brief Contains the main() function, where execution begins.
+ */
+
 #include <SDL3/SDL_main.h>
-#include <SDL3/SDL_messagebox.h>
-#include <SDL3/SDL_log.h>
+
+#include "engine.hpp"
 
 int main(int argc, char *argv[])
 {
-    SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, "Hello, world!");
-    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "Stirling Engine", "Hello, world!", nullptr);
-    return 0;
+    // Create and run an Engine
+    Engine engine(argc, argv);
+    engine.run();
+    return 0; // Success
 }
