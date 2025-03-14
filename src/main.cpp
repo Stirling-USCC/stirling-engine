092da29daa7efec048f655c8b7e18d96179894d8
Michael Connor Buchan <mikey@blindcomputing.org>
2025-03-14 22:45:48 +0000

    feat: Log and show message box when exceptions are thrown

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,26 @@
  * @brief Contains the main() function, where execution begins.
  */
 
+#include <exception>
+
+#include <SDL3/SDL_log.h>
 #include <SDL3/SDL_main.h>
+#include <SDL3/SDL_messagebox.h>
 
 #include "engine.hpp"
 
 int main(int argc, char *argv[]) {
-  // Create and run an Engine
-  Engine engine(argc, argv);
-  engine.run();
+  try {
+    // Create and run an Engine
+    Engine engine(argc, argv);
+    engine.run();
+  } catch (const std::exception &e) {
+    // Display a message box with the error
+    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Critical Error", e.what(),
+                             nullptr);
+    // Log the error using SDL's logging system, in case the message box fails
+    SDL_LogCritical(SDL_LOG_CATEGORY_ERROR, "%s", e.what());
+    return 1; // Failure
+  }
   return 0; // Success
 }
